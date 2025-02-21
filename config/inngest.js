e23ff84c7d8a3cd037de43835c6459876d79a9c9
dev02 <142491843+golffer420014@users.noreply.github.com>
2025-02-21 21:40:40 +0700

    fixed prisma

--- config/inngest.js
+++ config/inngest.js
@@ -7,59 +7,56 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 
 // inngest fnc to save user data to a database
 export const syncUserCreation = inngest.createFunction(
-    {
-        id: "sync-user-from-clerk",
-    },
+    { id: "sync-user-from-clerk" },
     { event: "clerk/user.created" },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        
+
         const userData = {
-            id: id,  
+            id,
             email: email_addresses[0].email_address,
             name: `${first_name} ${last_name}`,
             imageUrl: image_url,
         };
 
-        await prisma.users.create({
-            data: userData,
+        await prisma.users.upsert({
+            where: { id },
+            update: userData,
+            create: userData,
         });
     }
 );
+
 
 // inngest fnc to save cart data to a database
 export const syncUserUpdation = inngest.createFunction(
-    {
-        id: "update-user-from-clerk",
-    },
+    { id: "update-user-from-clerk" },
     { event: "clerk/user.updated" },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        
+
         const userData = {
-            id: id,
+            id,
             email: email_addresses[0].email_address,
             name: `${first_name} ${last_name}`,
             imageUrl: image_url,
         };
 
-        await prisma.users.update({
+        await prisma.users.upsert({
             where: { id },
-            data: userData,
+            update: userData,
+            create: userData,
         });
     }
 );
 
+
 // inngest fnc to delete user data from a database
 export const syncUserDeletion = inngest.createFunction(
-    {
-        id: "delete-user-from-clerk",
-    },
+    { id: "delete-user-from-clerk" },
     { event: "clerk/user.deleted" },
     async ({ event }) => {
         const { id } = event.data;
-        await prisma.users.delete({
-            where: { id },
-        });
+        await prisma.users.deleteMany({ where: { id } });
     }
 );
